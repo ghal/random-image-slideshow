19e822204d6d4153838d8f810bc4de477099187f
Giannis Halvatzakis <ghalvatzakis2@gmail.com>
2013-12-12 18:10:23 +0200

    Not the same image after the loop

--- random_image_slideshow.js
+++ random_image_slideshow.js
@@ -1,4 +1,5 @@
 var myVar=setInterval(function(){myTimer()},4000); //timer is set to 4 seconds
+var j=-1;
 function myTimer(){
 	var img_number = 3; //enter the number of the images
 	var img=["img1_path", //add the path of the images
@@ -7,8 +8,11 @@ function myTimer(){
 	var link=["link_1", //enter the links for when you click an image
 	"link_2",
 	"link_3"];
+
 	var i = Math.floor(Math.random()*img_number); 
-	var txt="<a id=\"id1\"href=\""+link[i]+"\"><img src=\""+img[i]+"\"</a>";
+	while(i==j){j  = Math.floor(Math.random()*img_number);}
+	var txt="<a href=\""+link[i]+"\"><img src=\""+img[i]+"\"</a>";
 	document.getElementById("document_id").innerHTML=txt; //Replace document_id with an id
+	j=i;
 }
 
